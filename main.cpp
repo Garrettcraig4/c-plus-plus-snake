4162408f7c4b92f57fda7e49ec91633f39805ab6
Garrett <Garrettcrg@gmail.com>
2018-05-01 16:12:39 -0500

    added-draw-to-game

--- main.cpp
+++ main.cpp
@@ -42,7 +42,12 @@ void Draw()
 
             if (j == 0)
                 cout << "#";
-            cout << " ";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
+                cout << " ";
 
             if (j == width - 1)
                 cout << "#";
